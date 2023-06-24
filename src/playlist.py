8d6e3d106a2ba9cb5f3369120dd998480949e3fb
Valerya Yakushina <v.yaku.bmstu@gmail.com>
2023-06-24 16:51:26 +0300

    create class PlayList

---
+++ src/playlist.py
@@ -0,0 +1,34 @@
+import os
+from googleapiclient.discovery import build
+
+
+class PlayList:
+    """Класс для плейлиста"""
+    api_key: str = os.getenv('YT_API_KEY')
+
+    def __init__(self, playlist_id):
+        self.__playlist_id = playlist_id
+        self.title = self.get_playlist_title()
+        self.url = ''.join(['https://www.youtube.com/playlist?list=', self.__playlist_id])
+
+    def get_info(self):
+        """По id плейлиста получает данные о элементах плейлиста"""
+        playlist = self.get_service().playlistItems().list(part='snippet,contentDetails',
+                                                           playlistId=self.__playlist_id
+                                                           ).execute()
+        return playlist
+
+    def get_playlist_title(self):
+        """По id канала получает список плейлистов на канале и из них получает название канала по его id"""
+        playlists = self.get_service().playlists().list(channelId=self.get_info()['items'][0]['snippet']['channelId'],
+                                                        part='contentDetails,snippet',
+                                                        maxResults=50,
+                                                        ).execute()
+
+        for playlist in playlists['items']:
+            if playlist['id'] == self.__playlist_id:
+                return playlist['snippet']['title']
+
+    @classmethod
+    def get_service(cls):
+        return build('youtube', 'v3', developerKey=cls.api_key)
